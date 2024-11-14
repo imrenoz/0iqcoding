702d43d72745e5d71f26ac29fb1dd11a4a21f469
notfound <125207685+imrenoz@users.noreply.github.com>
2024-11-14 16:12:05 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from pydantic import BaseSettings
+from pydantic.v1 import BaseSettings
 
-# Класс для загрузки настроек из .env файла
 class Settings(BaseSettings):
     DATABASE_PORT: int
     POSTGRES_PASSWORD: str
@@ -11,15 +8,10 @@ class Settings(BaseSettings):
     POSTGRES_HOST: str
     POSTGRES_HOSTNAME: str
 
+
     class Config:
-        env_file = '.env'  # Указываем, что настройки должны загружаться из .env файла
+        env_file = '.env'
+
 
 settings = Settings()
 
-# Формируем строку подключения к базе данных
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@" \
-                          f"{settings.POSTGRES_HOST}:{settings.DATABASE_PORT}/{settings.POSTGRES_DB}?sslmode=require"
-
-# Создание SQLAlchemy движка и сессии
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
